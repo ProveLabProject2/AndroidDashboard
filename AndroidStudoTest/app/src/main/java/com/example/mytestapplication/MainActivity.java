b2b91abba228e8f9d9f85b70932d8b17608b7738
VasanthSadhasivan <vasanthsadhasivan@gmail.com>
2020-12-05 19:37:03 -0800

    Added permissions to app

--- AndroidStudoTest/app/src/main/java/com/example/mytestapplication/MainActivity.java
+++ AndroidStudoTest/app/src/main/java/com/example/mytestapplication/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.mytestapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
@@ -27,40 +34,79 @@ public class MainActivity extends AppCompatActivity implements SerialInputOutput
 
     private static final String TAG = "MainActivity";
 
+    private static final String ACTION_USB_PERMISSION =
+            "com.android.example.USB_PERMISSION";
+
+    UsbSerialDriver driver;
+
+    UsbManager manager;
+
+    private final BroadcastReceiver usbReceiver = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if(device != null){
+                            Log.d(TAG, "Success");
+                            //call method to set up device communication
+                            UsbDeviceConnection connection = manager.openDevice(device);
+                            if (connection == null) {
+                                // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
+                                return;
+                            }
+
+                            Log.d(TAG, driver.getPorts().toString());
+                            UsbSerialPort port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+                            try {
+                                port.open(connection);
+                            } catch (IOException e) {
+                                Log.d(TAG, "onCreate0: ");
+                                e.printStackTrace();
+                            }
+                            try {
+                                port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+                            } catch (IOException e) {
+                                Log.d(TAG, "onCreate1: ");
+                                e.printStackTrace();
+                            }
+                            SerialInputOutputManager usbIoManager = new SerialInputOutputManager(port, (SerialInputOutputManager.Listener) context);
+                            Executors.newSingleThreadExecutor().submit(usbIoManager);
+                        }
+                    }
+                    else {
+                        Log.d(TAG, "permission denied for device " + device);
+                    }
+                }
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        PendingIntent permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        registerReceiver(usbReceiver, filter);
+
+
         List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
         if (availableDrivers.isEmpty()) {
             return;
         }
 
-        // Open a connection to the first available driver.
-        UsbSerialDriver driver = availableDrivers.get(0);
-        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
-        if (connection == null) {
-            // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
-            return;
-        }
 
-        Log.d(TAG, driver.getPorts().toString());
-        UsbSerialPort port = driver.getPorts().get(0); // Most devices have just one port (port 0)
-        try {
-            port.open(connection);
-        } catch (IOException e) {
-            Log.d(TAG, "onCreate0: ");
-            e.printStackTrace();
-        }
-        try {
-            port.setParameters(9600, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-        } catch (IOException e) {
-            Log.d(TAG, "onCreate1: ");
-            e.printStackTrace();
-        }
-        SerialInputOutputManager usbIoManager = new SerialInputOutputManager(port, this);
-        Executors.newSingleThreadExecutor().submit(usbIoManager);
+        // Open a connection to the first available driver.
+        driver = availableDrivers.get(0);
+
+        manager.requestPermission(driver.getDevice(), permissionIntent);
     }
 
 
